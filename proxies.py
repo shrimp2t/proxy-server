6870fda12d867248c2814a71f2ea13a8ca1cca89
TruongSa <shrimp2t@gmail.com>
2019-11-04 14:14:21 +0700

    Update proxies.py

--- proxies.py
+++ proxies.py
@@ -9,13 +9,16 @@ async def show(proxies):
         row = '%s://%s:%d\n' % (proto, proxy.host, proxy.port)
         print( '%s\n' % proxy )
 
-countries = ['US']
-proxies = asyncio.Queue()
-broker = Broker(proxies)
-tasks = asyncio.gather( 
-    broker.find(broker.find(types=['HTTP', 'HTTPS'], countries=countries, limit=10),
-    show(proxies))
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(tasks)
+def main():
+    countries = ['US']
+    proxies = asyncio.Queue()
+    broker = Broker(proxies)
+    tasks = asyncio.gather(broker.find(broker.find(types=['HTTP', 'HTTPS'], countries=countries, limit=10),
+                           show(proxies))
 
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(tasks)
+
+if __name__ == '__main__':
+    main()
