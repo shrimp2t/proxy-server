3848a75cf94ca4ed47e2924d3099cae676abbadb
TruongSa <shrimp2t@gmail.com>
2019-11-04 16:08:48 +0700

    Update app.js

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ let app = express();
 app.get('/', (req, res, next)=>{
 	var limit = (req.query.limit)?req.query.limit:10;
 	exec(`python3 proxies.py --limit ${limit}`,{windowsHide :true}, (err, stdout, stderr) => {
-	  if (err) {
-	    res.status(401).json({status: 'error', message: 'error', data: err});
-	    return;
-	  }
-	  var proxies = stdout.split('\n');
-	  console.log( 'proxies', proxies );
-	  res.json({status: 'success', message: 'Successfully', data: proxies});
+		if (err) {
+			res.status(401).json({status: 'error', message: 'error', data: err});
+			return;
+		}
+		var proxies = stdout.split('\n');
+		console.log( 'proxies', proxies );
+		res.json({status: 'success', message: 'Successfully', data: proxies});
 	});
 });
 
